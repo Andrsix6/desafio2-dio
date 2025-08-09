f68403450fe56c35d5eae34fe607be908ce825ca
Andrsix6 <vinigamer1905@gmail.com>
2025-08-09 02:03:09 -0300

    versão 1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+let resultado = saldoVitorias(1000, 3);
+
+// console.log(resultado);
+
+function saldoVitorias(vitorias, derrotas){
+    return vitorias - derrotas;
+}
+
+let nivel = "";
+if (resultado < 10){
+        nivel = "Ferro"
+    }else if(resultado >= 11, resultado <= 20){
+        nivel = "Bronze"
+    }else if(resultado >= 21, resultado <= 50){
+        nivel = "Prata"
+    }else if(resultado >= 51, resultado <= 80){
+        nivel = "Ouro"
+    }else if(resultado >= 81, resultado <= 90){
+        nivel = "Diamante"
+    }else if(resultado >= 91, resultado <= 100){
+        nivel = "Lendário"
+    }else if(resultado >= 101){
+        nivel = "Imortal"
+    }
+    console.log("O Herói tem de saldo de " + resultado + " está no nível de " + nivel);
